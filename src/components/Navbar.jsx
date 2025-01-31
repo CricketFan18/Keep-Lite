7c1b026b1a1e91ce57293f23e3e240b944d43461
Vivek Kesarwani <vivekkesarwani2005@gmail.com>
2025-02-01 01:06:35 +0530

    Added favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import "./Navbar.css"
+import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <h1>Notes App </h1> <span className="material-symbols-outlined" style= {{marginLeft: "0.25em" , fontSize: "2.75rem" , position:"relative" , top: "0.4rem"}}>edit_note</span>
+    <div className="navbar">
+      <h1>Keep Lite </h1>{" "}
+      <span
+        className="material-symbols-outlined"
+        style={{
+          marginLeft: "0.25em",
+          fontSize: "2.75rem",
+          position: "relative",
+          top: "0.4rem",
+        }}
+      >
+        edit_note
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
